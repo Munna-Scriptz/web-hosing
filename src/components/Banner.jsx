a0ff734fcbedbfcfd1226beb2fca9ddead16df3f
Munna-Scripts <munnascriptz@gmail.com>
2025-08-28 16:49:00 +0600

    Navbar And Banner Complete

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,46 +1,63 @@
-
 import React from 'react'
-import { Link } from 'react-router'
-import { FaAngleDown } from "react-icons/fa6";
-import Graph1 from '../assets/images/BannerGraph1.png'
-import Graph2 from '../assets/images/BannerGraph2.png'
-import ScholarShip from '../assets/images/BannerImg.png'
-import Percentage from '../assets/images/Percentage.png'
+import { IoSearchOutline } from 'react-icons/io5'
+import domain1 from '../assets/images/domain1.png'
+import domain2 from '../assets/images/infodomain.svg'
+import domain3 from '../assets/images/domain3.webp'
+import domain4 from '../assets/images/domain4.webp'
+import domain5 from '../assets/images/domain5.png'
+import BannerImage from '../assets/images/bannerRightImg.svg'
 
 const Banner = () => {
   return (
     <>
       <section id='Banner' className='mt-[65px]'>
         <div className="container">
-          {/* ---------------Row One----------------- */}
-          <div id="BannerRowOne" className='flex items-center justify-between'>
-            <h1 className='font-inter font-bold text-secondBK text-[75px] w-[857px]'>Expand Your Horizons through <span className='text-[#2F57EF]'>Education</span></h1>
+          <div id='Banner-Row' className='flex items-center justify-between'>
+            {/* --------------Left Side------------ */}
             <div>
-              <p className='font-inter text-[21px] text-secondBK mb-[49px]'>Education for a Better Tomorrow.</p>
-              <Link className='py-[25px] px-[45px] border-1 rounded-[5px] border-primary font-inter font-medium text-[15px] text-secondBK' to={'/'}>Contact Us</Link>
-            </div>
-          </div>
-          {/* ---------------Row One----------------- */}
-          <div id='BannerRowTwo' className='flex items-center justify-between mt-[78px]'>
-            <div className='flex flex-col items-center gap-[30px]'>
-              <img src={Graph1} alt="Graph" />
-              <img src={Graph2} alt="Graph" />
-            </div>
-            <div className='rounded-b-[20px]'>
-              <img src={ScholarShip} alt="ScholarShip IMage" />
-            </div>
-            <div className='flex flex-col '>
+              {/* ---------------Row One----------------- */}
               <div>
-                <h2 className='text-[#233A45] text-[18px] font-inter font-medium'>Lorem ipsum</h2>
-                <p className='text-[#7C7D93] font-inter text-base mb-[20px]'>Lorem ipsum dolor sit amet</p>
+                <h1 className='font-inter font-bold text-secondBK text-[48px] w-[500px]'>Search For Your Own Unique <span className='text-brand'>Domain !</span></h1>
+                <p className='font-inter text-[#969696] mt-5 w-[500px] text-lg'>Searching for that perfect domain? Progressively benchmark turnkey innovation after quality channels.</p>
               </div>
-              <div> 
-                <img src={Percentage} alt="Image" />
+              {/* ---------------Input----------------- */}
+              <div className='mt-10'>
+                <div className='relative w-[600px] h-[60px]'>
+                  <input className='w-full h-full border-1 border-[#19233533] outline-none rounded-[8px] pl-[20px] text-secondBK font-inter' placeholder='Example.com' type="text"/>
+                  <button className='absolute top-0 right-0 h-full w-[120px] rounded-r-[8px] cursor-pointer font-inter bg-brand text-white flex items-center justify-center gap-2'><IoSearchOutline className='text-lg'/>Search</button>
+                </div>
+                {/* -----------Domain Show and starting------------- */}
+                <div className='mt-5 flex items-center gap-5'>
+                  <div className='flex flex-col items-center'>
+                    <img className='w-[70px] h-[30px]' src={domain4} alt="domain" />
+                    <p className='font-inter text-[#838383] text-sm font-medium mt-1'>$9.99</p>
+                  </div>
+                  <div className='flex flex-col items-center'>
+                    <img className='w-[90px] h-[30px]' src={domain1} alt="domain" />
+                    <p className='font-inter text-[#838383] text-sm font-medium mt-1'>$7.99</p>
+                  </div>
+                  <div className='flex flex-col items-center'>
+                    <img className='w-[90px] h-[30px]' src={domain3} alt="domain" />
+                    <p className='font-inter text-[#838383] text-sm font-medium mt-1'>$5.99</p>
+                  </div>
+                  <div className='flex flex-col items-center'>
+                    <img className='w-[60px] h-[30px]' src={domain2} alt="domain" />
+                    <p className='font-inter text-[#838383] text-sm font-medium mt-1'>$9.99</p>
+                  </div>
+                  <div className='flex flex-col items-center'>
+                    <img className='w-[70px] h-[30px]' src={domain5} alt="domain" />
+                    <p className='font-inter text-[#838383] text-sm font-medium mt-1'>$9.99</p>
+                  </div>
+                </div>
+                <div className='mt-8'>
+                  <p className='text-secondBK font-inter font-medium'>Starting At Only</p>
+                  <h2 className='text-secondBK font-inter text-3xl mt-2 font-semibold'>$2.78/<span className='text-brand'>mo</span></h2>
+                </div>
               </div>
-              <div>
-                <p className='text-[#7C7D93] font-inter text-base w-[210px] my-[24px]'>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-                <Link className='bg-[#FFD465] w-[192px] h-[39px] flex items-center justify-center rounded-full text-[#233A45] font-medium' to={'/'}>Subscription</Link>
-              </div>
+            </div>
+            {/* --------------Right Side------------ */}
+            <div>
+              <img className='w-[550px]' src={BannerImage} alt="Banner Image" />
             </div>
           </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import { FaAngleDown } from "react-icons/fa6";
 import { IoSearchSharp } from "react-icons/io5";
 import { MdOutlineShoppingCart } from "react-icons/md";
 
-import Logo from '../assets/images/Logo.png'
+import Logo from '../assets/images/domainHostingLogo.png'
+import { Link } from 'react-router';
 const Navbar = () => {
   return (
     <header>
@@ -16,11 +17,11 @@ const Navbar = () => {
             <div className='flex items-center justify-between'>
               <div className="flex items-center gap-2 text-secondBK font-inter text-[17px]">
                 <FaLocationDot />
-                <p>6391 Elgin St. Celina, 10299</p>
+                <p>Dhaka , Bangladesh</p>
               </div>
               <div className="flex items-center gap-[31px] font-inter text-[17px] text-secondBK">
                 <p className='flex items-center gap-1'><IoCall /> (629) 555-0129</p>
-                <p className='flex items-center gap-1'><MdEmail /> info@example.com</p>
+                <p className='flex items-center gap-1'><MdEmail />munnascriptz@gmail.com</p>
               </div>
             </div>
           </div>
@@ -28,22 +29,18 @@ const Navbar = () => {
       {/* ----------------Navbar Section----------------- */}
       <div className="container">
         <nav className="flex items-center justify-between py-4">
-          <div className='flex items-center gap-[32px]'>
-            <img src={Logo} alt="Logo" />
+          <div className='flex items-center gap-[82px]'>
+            <Link to={'/'}><img className='w-[100px]' src={Logo} alt="Logo" /></Link>
             <ul className="flex items-center gap-[24px] font-inter text-base text-secondBK font-medium">
               <li className="hover:text-blue-600 cursor-pointer flex items-center gap-1">Home <FaAngleDown className='text-[10px]'/></li>
               <li className="hover:text-blue-600 cursor-pointer flex items-center gap-1">About Us <FaAngleDown className='text-[10px]'/></li>
               <li className="hover:text-blue-600 cursor-npm run devointer flex items-center gap-1">Services <FaAngleDown className='text-[10px]'/></li>
-              <li className="hover:text-blue-600 cursor-pointer flex items-center gap-1">Projects <FaAngleDown className='text-[10px]'/></li>
-              <li className="hover:text-blue-600 cursor-pointer flex items-center gap-1">Blog <FaAngleDown className='text-[10px]'/></li>
-              <li className="hover:text-blue-600 cursor-pointer flex items-center gap-1">Page <FaAngleDown className='text-[10px]'/></li>
               <li className="hover:text-blue-600 cursor-pointer flex items-center gap-1">Contact <FaAngleDown className='text-[10px]'/></li>
             </ul>
           </div>
-          <div className="flex items-center gap-4">
-            <input type="text" placeholder="Search" className="w-[293px] h-[60px] outline-none border-1 border-[#19233533] pl-[20px] rounded-[5px]" />
-            <button className="h-[60px] w-[60px] bg-primary rounded-[5px] flex items-center justify-center text-white text-[20px] cursor-pointer"><IoSearchSharp /></button>
-            <button className="h-[60px] w-[60px] hover:bg-primary duration-[.3s] text-primary rounded-[5px] flex items-center justify-center hover:text-white text-[20px] cursor-pointer"><MdOutlineShoppingCart  /></button>
+          <div className="flex items-center gap-3">
+            <input type="text" placeholder="Search domain..." className="w-[293px] h-[50px] outline-none font-inter border-1 border-[#19233533] pl-[20px] rounded-[5px]" />
+            <button className="h-[50px] w-[60px] bg-primary rounded-[5px] flex items-center justify-center text-white text-[20px] cursor-pointer"><IoSearchSharp /></button>
           </div>
         </nav>
       </div>
